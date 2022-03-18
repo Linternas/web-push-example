43ceaf509036d5eda350022875f4f85f65e2aea8
Linternas <kmg20000215@gmail.com>
2022-03-18 21:22:05 +0900

    push 수정

--- push.js
+++ push.js
@@ -7,5 +7,12 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.co.kr', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {}
+let sub = {
+  "endpoint": "https://fcm.googleapis.com/fcm/send/eKCdMEFydcQ:APA91bEI_ekSuDi0TAAjDJY5eiObu-ZlmvD-_zADjH_-r_LmmMmLiOAsimlwzRPKU9Bs5wMEuahlfvt4dX-zmEnOrOZIRiiqOa5Xype3RzJT58UciS68AxmdzAQDu0eqwNUrJZpvv7rc",
+  "expirationTime": null,
+  "keys": {
+    "p256dh": "BLYkqJl4AeumWqoyeDbsH_OsAw3RXWRfeoCzBIvPJvEu_CoAdpfbT7Zm2R3ttEj7E4X-aLZpwSJ1SnBdD-yqZx0",
+    "auth": "bxG_QL3BkZAo-i8VE6JFMw"
+  }
+}
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
 self.addEventListener('push', () => {
-  self.registration.sendNotification('test message', {})
+  // self.registration.sendNotification('test message', {})
+
+  var option = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {
+        actions: 'explore',
+        title: 'Explore this new world',
+        icon: 'iamges/checkmark.png'
+      }, { actions: 'close', title: 'Close', icon: 'images/xmark.png' }
+    ]
+  }
+
+  e.waituntil(self.registration.showNotification('Hello world', options))
 })
